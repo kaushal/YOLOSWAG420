9bf8eaeb5462f6330af7510ab5abc8d6f31548a4
Eddie Zaneski <eddiezane@gmail.com>
2013-04-20 15:36:40 -0400

    Dis shiz be workin YOLO

--- app.rb
+++ app.rb
@@ -1,15 +1,20 @@
 require 'sinatra'
 require 'twitter'
 
-
 Twitter.configure do |config|
   config.consumer_key = ENV['CONKEY']
   config.consumer_secret = ENV['CONSEC']
   config.oauth_token = ENV['OAUTHTOKEN']
   config.oauth_token_secret = ENV['OAUTHSECRET']
 end
-  puts Twitter.search('#yoloswag420', count: 1)
 
 get '/' do
-  Twitter.search('#yoloswag420', count: 1).results.first.text
+  @tweets = Twitter.search('#yoloswag420', count: 100).results
+  erb :index
 end
+
+__END__
+@@index
+<% @tweets.each do |tweet| %>
+  <p><%= "@#{tweet.from_user}: #{tweet.text}" %></p>
+<% end %>
