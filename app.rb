71bf00040c9cbc7fec5b9d06a5aa90bcceeb1809
Ian Lozinski <ian.lozinski@gmail.com>
2013-04-20 14:49:08 -0400

    gettin twizzzeeets #YOLOSWAG420

--- app.rb
+++ app.rb
@@ -1,6 +1,15 @@
 require 'sinatra'
 require 'twitter'
 
+
+Twitter.configure do |config|
+  config.consumer_key = ENV['CONKEY']
+  config.consumer_secret = ENV['CONSEC']
+  config.oauth_token = ENV['OAUTHTOKEN']
+  config.oauth_token_secret = ENV['OAUTHSECRET']
+end
+  puts Twitter.search('#yoloswag420', count: 1)
+
 get '/' do
-  'Hello World'
+  Twitter.search('#yoloswag420', count: 1).results.first.text
 end
